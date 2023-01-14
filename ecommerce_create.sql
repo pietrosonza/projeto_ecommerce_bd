42fbe0a90c177b872c10b6fe61f473455e2a4780
pietrosonza <101946047+pietrosonza@users.noreply.github.com>
2023-01-14 09:44:33 -0300

    Update ecommerce_create.sql

--- ecommerce_create.sql
+++ ecommerce_create.sql
@@ -6,39 +6,39 @@ drop database ecommerce;
 -- criando a tabela Cliente
 create table cliente(
 	idCliente int auto_increment primary key,
-    endereco varchar(60) not null,
-    contato varchar(45) not null
+    	endereco varchar(60) not null,
+    	contato varchar(45) not null
     );
 
 -- Criando a tabela clientepf
 create table clientePF(
 	idCliente_pf int auto_increment primary key,
-    idCliente int not null,
-    pNome varchar(20) not null,
-    mNome varchar(3) not null,
-    fNome varchar(20) not null,
-    cpf char (11) not null,
-    nascimento date not null,
-    sexo char (1) not null,
-    dataCadastro date not null,
-    constraint pf_cpf_unique unique (CPF, idCliente),
-    constraint fk_pf_cliente_geral foreign key(idCliente) references cliente(idCliente));
+    	idCliente int not null,
+    	pNome varchar(20) not null,
+    	mNome varchar(3) not null,
+    	fNome varchar(20) not null,
+    	cpf char (11) not null,
+    	nascimento date not null,
+    	sexo char (1) not null,
+    	dataCadastro date not null,
+    	constraint pf_cpf_unique unique (CPF, idCliente),
+    	constraint fk_pf_cliente_geral foreign key(idCliente) references cliente(idCliente));
 
 -- Criando a tabela clientepj
 create table clientePJ(
 	idCliente_pj int auto_increment primary key,
-    idCliente int not null,
+	idCliente int not null,
 	razaoSocial varchar(45) not null,
-    nomeFantasia varchar(45),
-    cnpj varchar(20) not null,
-    ramo varchar(25),
-    porte enum('p','m','g'),
-    constraint pj_cnpj_unique unique (cnpj, idCliente),
-    constraint fk_pj_cliente_geral_pj foreign key (idCliente) references cliente(idCliente));
+	nomeFantasia varchar(45),
+	cnpj varchar(20) not null,
+	ramo varchar(25),
+	porte enum('p','m','g'),
+	constraint pj_cnpj_unique unique (cnpj, idCliente),
+ 	constraint fk_pj_cliente_geral_pj foreign key (idCliente) references cliente(idCliente));
 
 -- criando a tabela Produto
 create table produto(
-		idProduto int auto_increment primary key,
+	idProduto int auto_increment primary key,
         pNome varchar(40) not null,
         categoria enum('Eletronico', 'Vestuario', 'Esportes', 'Móveis') not null,
         marca varchar(20),
@@ -55,27 +55,26 @@ create table pagamento(
 
 -- criando a tabela Pedido
 create table pedido(
-		idPedido int auto_increment primary key,
+	idPedido int auto_increment primary key,
         idCliente int,
         idPagamento int,
         statusPedido enum('Processando', 'Em andamento', 'Enviado') default 'Processando',
         frete float default 10,
-        constraint fk_pedido_Cliente foreign key (idPedido) references cliente(idCliente)
-				on update cascade,
+        constraint fk_pedido_Cliente foreign key (idPedido) references cliente(idCliente),
         constraint fk_pedido_Pagamento foreign key (idPagamento) references pagamento (idPagamento)
         
 );
 
 -- criando a tabela estoque
 create table estoque (
-		idEstoque int auto_increment primary key,
+	idEstoque int auto_increment primary key,
         endereco varchar(60) not null
 );
 
 -- criando a tabela fornecedor
 create table fornecedor (
-		idFornecedor int auto_increment primary key,
-		cnpj char(18) not null,
+	idFornecedor int auto_increment primary key,
+	cnpj char(18) not null,
         razaoSocial varchar(45) not null,
         nomeFantasia varchar(45),
         endereco varchar(60) not null,
@@ -85,7 +84,7 @@ create table fornecedor (
 
 -- criando tabela de produto_estoque - gerada através de uma relação n:m
 create table produtoEstoque (
-		idEstoque int,
+	idEstoque int,
         idProduto int,
         quantidadeEstoque int not null,
         constraint pk_produtoEstoque primary key (idEstoque, idProduto),
@@ -95,7 +94,7 @@ create table produtoEstoque (
 
 -- criando tabela de produto_pedido - gerada através de uma relação n:m
 create table produtoPedido (
-		idPedido int,
+	idPedido int,
         idProduto int,
         quantidade int default 1,
         statusProduto enum('disponivel','esgotado') default 'disponivel',
@@ -107,15 +106,15 @@ create table produtoPedido (
 -- criando tabela de produto_fornecedor - gerada através de uma relação n:m
 create table produtoFornecedor(
 	idFornecedor int,
-    idProduto int,
-    quantidade int not null,
-    constraint pk_prudutoFornecedor primary key (idFornecedor, idProduto),
-    constraint fk_produtoFornecedor_Fornecedor foreign key (idFornecedor) references fornecedor(idFornecedor),
-    constraint fk_produtoFornecedor_Produto foreign key (idProduto) references produto(idProduto)
+    	idProduto int,
+    	quantidade int not null,
+    	constraint pk_prudutoFornecedor primary key (idFornecedor, idProduto),
+    	constraint fk_produtoFornecedor_Fornecedor foreign key (idFornecedor) references fornecedor(idFornecedor),
+    	constraint fk_produtoFornecedor_Produto foreign key (idProduto) references produto(idProduto)
 );
 
 create table entrega(
 	idEntrega int auto_increment primary key,
-    idPedido int,
-    statusEntrega enum('Em transporte', 'Entregue'),
-    constraint pk_entrega_pedido foreign key (idPedido) references pedido(idPedido));
+	idPedido int,
+	statusEntrega enum('Em transporte', 'Entregue'),
+	constraint pk_entrega_pedido foreign key (idPedido) references pedido(idPedido));
